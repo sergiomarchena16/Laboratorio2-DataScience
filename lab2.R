1d219bdaabd2f93f28e99e7069aa440bec08d0f4
Sergio Marchena <mar16387@uvg.edu.gt>
2019-08-21 12:05:04 -0600

    dataset divido
    division del dataset

--- lab2.R
+++ lab2.R
@@ -1,5 +1,30 @@
 # LABORATORIO 2 DE DATA SCIENCE
 # SERGIO MARCHENA 16387
-# PABLO VIANA 16091
 
-# --------------------------- AVANCE -----------------------------
+
+# -------------------------------------------------- AVANCE ----------------------------------------------------
+
+datos<-read.csv("train.csv")
+
+
+# 1. DIVIDIR EL DATASET EN CONJUNTOS DE ENTRANAMIENTO Y DE PRUEBA 
+
+
+set.seed(123)
+
+#Porciento en el que se partirán los datos
+porcentaje <- 60/100 
+
+#Muestra aleatoria de numeros de un vector
+muestra<-sample(1:nrow(datos),porcentaje*nrow(datos))
+
+#Obtengo las filas de los elementos que estan en el sector de muestra
+trainSet<-datos[muestra,]
+
+#Obtengo las filas de los elementos que no están en el vector de muestra
+testSet<-datos[-muestra,] 
+
+
+# 2. MODELO DE REGRESION LINEAL
+
+
