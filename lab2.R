0d71122063b62bdfed9c6866bd3c25c8458f29da
Sergio Marchena <mar16387@uvg.edu.gt>
2019-08-27 00:10:05 -0600

    hola
    hola

--- lab2.R
+++ lab2.R
@@ -1,12 +1,16 @@
 # LABORATORIO 2 DE DATA SCIENCE
 # SERGIO MARCHENA 16387
+# JORGE CHICAS 14617
 
 
-# -------------------------------------------------- AVANCE ----------------------------------------------------
 install.packages("caret")
 library(caret)
 library(dplyr)
+library(Hmisc)
+library(class)
+
 datos<-read.csv("train.csv")
+prueba<-read.csv("test.csv")
 
 
 # 1. DIVIDIR EL DATASET EN CONJUNTOS DE ENTRANAMIENTO Y DE PRUEBA 
@@ -32,17 +36,58 @@ testSet<-datos[-muestra,]
 #--- SIMPLE
 
 #Generacion del Modelo Lineal
-modeloLinealSimple<-lm(SalePrice~OverallQual,GarageArea,GrLivArea, data = trainSet)
+
+modeloLinealSimple<-lm(SalePrice~OverallQual, data = trainSet)
+
 # Aqui comparamos la variable de SalePrice contra OverallQual que segun el Analisis Exploratorio, 
 # es una de las variables con mas correlacion. 
 summary(modeloLinealSimple)
 
 #Prediccion
-prediccion<-predict(modeloLinealSimple, newdata = testSet[2:80])
+prediccion<-predict(modeloLinealSimple, newdata = testSet)
 # La prediccion se hace con el modelo creado anteriormente y el nuevo data para probar son
 # las columnas de la 1-80, y no a la 81, que es la que se quiere predecir. 
 
 prediccion
+# Se agrega la prediccion al conjunto de test
+testSet$SalePred<-prediccion
+View(testSet[81:82])
+
+#Ver la diferencia entre lo real y lo predicho
+dif<-abs(testSet$SalePred-testSet$SalePrice)
+View(dif)
+
+promedioError<- mean(dif)
+summary(dif)
+promedioError
+
+# Matriz de Confusion
+real<-as.factor(testSet$SalePrice)
+str(real)
+prede<-as.factor(testSet$SalePred)
+str(prede)
+cfm<-confusionMatrix(real,prede)
+cfm
+
+testSet$SalePred <-NULL
+
+#--- MULTIVARIADA
+
+#Generacion del Modelo Lineal Multivariado
+nums<-dplyr::select_if(trainSet, is.numeric)   #SOLO VARIABLES NUMERICAS
+# primero se separan las variables numericas, ya que no funciona para variables con muchos 
+# niveles. 
+modeloLinealMulti<-lm(as.numeric(SalePrice)~., data = nums)
+# Luego, se trata de comparar la variable de SalePrice contra todas las demas variables numericas
+# para ver si hay regresion multivariada.
+View(modeloLinealMulti)
+
+#Prediccion
+prediccion<-predict(modeloLinealMulti,newdata = testSet[1:80])
+# Se hace la comparacion usando el modelo multivariado en el testSet y se seleccionan todas las
+# variables menos la ultima, que es la que se quiere predecir. 
+
+na.omit(prediccion)
 
 # Se agrega la prediccion al conjunto de test
 testSet$SalePred<-prediccion
@@ -60,53 +105,11 @@ promedioError
 
 real<-as.factor(testSet$SalePrice)
 str(real)
+
 prede<-as.factor(testSet$SalePred)
 str(prede)
-cfm<-confusionMatrix(real,prede)
+cfm<-confusionMatrix(as.factor(real),testSet$SalePred)
 cfm
 
 testSet$SalePred <-NULL
-
-#--- MULTIVARIADA
-
-#Generacion del Modelo Lineal Multivariado
-nums<-dplyr::select_if(trainSet, is.numeric)   #SOLO VARIABLES NUMERICAS
-# primero se separan las variables numericas, ya que no funciona para variables con muchos 
-# niveles. 
-modeloLinealMulti<-lm(SalePrice~., data = nums)
-# Luego, se trata de comparar la variable de SalePrice contra todas las demas variables numericas
-# para ver si hay regresion multivariada.
-View(modeloLinealMulti)
-
-#Prediccion
-prediccion<-predict(modeloLinealMulti,newdata = testSet[1:80])
-# Se hace la comparacion usando el modelo multivariado en el testSet y se seleccionan todas las
-# variables menos la ultima, que es la que se quiere predecir. 
-
-prediccion
-
-# Se agrega la prediccion al conjunto de test
-testSet$SalePred<-prediccion
-View(testSet[81:82])
-
-#Ver la diferencia entre lo real y lo predicho
-dif<-abs(testSet$SalePred-testSet$SalePrice)
-View(dif)
-
-promedioError<- mean(dif)
-promedioError
-
-# Matriz de Confusion
-
-real<-as.integer(testSet$SalePrice)
-str(real)
-
-prede<-as.integer(testSet$SalePred)
-str(prede)
-cfm<-confusionMatrix(as.factor(real),as.factor(prede))
-cfm
-
-testSet$SalePred <-NULL
-
-# ---------------------------------------------- FIN DE AVANCE ----------------------------------------------------
 
